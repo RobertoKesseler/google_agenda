49fb1b400262d377b4789be5924bab0e883c64d3
Roberto Kesseler <r.kesselerkesseler@gmail.com>
2021-08-11 15:06:55 -0300

    agenda_de_contatos

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:google_agenda/screens/home/home.dart';
+import 'package:google_agenda/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+    theme: lightTheme,
+  ));
+}
